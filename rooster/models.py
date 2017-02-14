3b9e235d81bdfa7f2c927cc73fa0f0233c48b1e0
Sander Adam <S.Adam@AVFlexologicbv.local>
2017-02-14 20:40:54 +0100

    added percentage 2

--- rooster/models.py
+++ rooster/models.py
@@ -13,7 +13,7 @@ class Dienst(models.Model):
     date = models.DateField()
     begintijd = models.TimeField()
     eindtijd = models.TimeField()
-    percentage = models.FloatField(default = 1)
+    percentage = models.FloatField()
 
     def __str__(self):
         return self.beschrijving + " " + self.date.strftime("%Y-%m-%d")
