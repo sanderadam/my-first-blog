f01ea334d7c5debb0a5fc6a279a23e3f4e697c3a
Sander Adam <S.Adam@AVFlexologicbv.local>
2017-04-28 13:08:37 +0200

    Removed WSGI from urls.py

--- rooster/urls.py
+++ rooster/urls.py
@@ -5,21 +5,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth import views as auth_views
 from .models import Dienst
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = Dienst.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 
 
 urlpatterns = [
